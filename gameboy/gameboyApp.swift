5a313e380c5a97044cbd55c56cf17352249e4ea0
franksalgado <54812935+franksalgado@users.noreply.github.com>
2022-12-27 23:17:37 -0800

    Initial Commit

---
+++ gameboy/gameboyApp.swift
@@ -0,0 +1,20 @@
+//
+//  gameboyApp.swift
+//  gameboy
+//
+//  Created by Frank Salgado on 12/27/22.
+//
+
+import SwiftUI
+
+@main
+struct gameboyApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
