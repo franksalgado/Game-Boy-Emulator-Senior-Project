cdfe41af80d9f5cb09792bc3ba525dee232a4e3b
franksalgado <54812935+franksalgado@users.noreply.github.com>
2023-03-26 01:44:37 -0700

    Implemented 21 more instructions

--- GameBoyEmulatorSeniorProject2023/Emulator/Instructions.swift
+++ GameBoyEmulatorSeniorProject2023/Emulator/Instructions.swift
@@ -112,7 +112,12 @@ func DECB() -> Void {
     SetFlagsRegister(z: equalToZero , n: 1, h: halfCarry, c: 2)
 }
 
-
+//0x06
+func LDBd8() -> Void {
+    CPUStateInstance.registersState.b = BusRead(address: CPUStateInstance.registersState.pc);
+    //emulator cycles
+    CPUStateInstance.registersState.pc+=1;
+}
 
 
 // This function increments C register by 1. 0x0C
@@ -160,6 +165,12 @@ func DECC() -> Void {
     SetFlagsRegister(z: equalToZero , n: 1, h: halfCarry, c: 2)
 }
 
+//0x0E
+func LDCd8() -> Void {
+    CPUStateInstance.registersState.c = BusRead(address: CPUStateInstance.registersState.pc);
+    //emulator cycles
+    CPUStateInstance.registersState.pc+=1;
+}
 
 
 
@@ -213,6 +224,13 @@ func DECD() -> Void {
     SetFlagsRegister(z: equalToZero , n: 1, h: halfCarry, c: 2)
 }
 
+//0x16
+func LDDd8() -> Void {
+    CPUStateInstance.registersState.d = BusRead(address: CPUStateInstance.registersState.pc);
+    //emulator cycles
+    CPUStateInstance.registersState.pc+=1;
+}
+
 
 // This function increments E register by 1. 0x1C
 func INCE() -> Void {
@@ -259,7 +277,12 @@ func DECE() -> Void {
     SetFlagsRegister(z: equalToZero , n: 1, h: halfCarry, c: 2)
 }
 
-
+//0x1E
+func LDEd8() -> Void {
+    CPUStateInstance.registersState.e = BusRead(address: CPUStateInstance.registersState.pc);
+    //emulator cycles
+    CPUStateInstance.registersState.pc+=1;
+}
 
 
 
@@ -308,6 +331,13 @@ func DECH() -> Void {
     SetFlagsRegister(z: equalToZero , n: 1, h: halfCarry, c: 2)
 }
 
+//0x26
+func LDHd8() -> Void {
+    CPUStateInstance.registersState.h = BusRead(address: CPUStateInstance.registersState.pc);
+    //emulator cycles
+    CPUStateInstance.registersState.pc+=1;
+}
+
 // This function increments L register by 1. 0x2C
 func INCL() -> Void {
     CPUStateInstance.registersState.l += 1;
@@ -351,6 +381,13 @@ func DECL() -> Void {
     }
     
     SetFlagsRegister(z: equalToZero , n: 1, h: halfCarry, c: 2)
+}
+
+//0x2E
+func LDLd8() -> Void {
+    CPUStateInstance.registersState.l = BusRead(address: CPUStateInstance.registersState.pc);
+    //emulator cycles
+    CPUStateInstance.registersState.pc+=1;
 }
 
 
@@ -397,6 +434,13 @@ func DECA() -> Void {
     }
     
     SetFlagsRegister(z: equalToZero , n: 1, h: halfCarry, c: 2);
+}
+
+//0x3E
+func LDAd8() -> Void {
+    CPUStateInstance.registersState.a = BusRead(address: CPUStateInstance.registersState.pc);
+    //emulator cycles
+    CPUStateInstance.registersState.pc+=1;
 }
 
 //Put the value of B into into B register 0x40
@@ -512,33 +556,107 @@ func LDEB() -> Void {
 
 // Put the value of C into E register 0x59
 func LDEC() -> Void {
-CPUStateInstance.registersState.e = CPUStateInstance.registersState.c;
+    CPUStateInstance.registersState.e = CPUStateInstance.registersState.c;
 }
 
 // Put the value of D into E register 0x5A
 func LDED() -> Void {
-CPUStateInstance.registersState.e = CPUStateInstance.registersState.d;
+    CPUStateInstance.registersState.e = CPUStateInstance.registersState.d;
 }
 
 // Put the value of E into E register 0x5B
 func LDEE() -> Void {
-CPUStateInstance.registersState.e = CPUStateInstance.registersState.e;
+    CPUStateInstance.registersState.e = CPUStateInstance.registersState.e;
 }
 
 // Put the value of H into E register 0x5C
 func LDEH() -> Void {
-CPUStateInstance.registersState.e = CPUStateInstance.registersState.h;
+    CPUStateInstance.registersState.e = CPUStateInstance.registersState.h;
 }
 
 // Put the value of L into E register 0x5D
 func LDEL() -> Void {
-CPUStateInstance.registersState.e = CPUStateInstance.registersState.l;
+    CPUStateInstance.registersState.e = CPUStateInstance.registersState.l;
 }
 
 // Put the value of A into E register 0x5F
 func LDEA() -> Void {
-CPUStateInstance.registersState.e = CPUStateInstance.registersState.a;
+    CPUStateInstance.registersState.e = CPUStateInstance.registersState.a;
 }
+
+// Put the value of B into H register 0x60
+func LDHB() -> Void {
+    CPUStateInstance.registersState.h = CPUStateInstance.registersState.b;
+}
+
+// Put the value of C into H register 0x61
+func LDHC() -> Void {
+    CPUStateInstance.registersState.h = CPUStateInstance.registersState.c;
+}
+
+// Put the value of D into H register 0x62
+func LDHD() -> Void {
+    CPUStateInstance.registersState.h = CPUStateInstance.registersState.d;
+}
+
+// Put the value of E into H register 0x63
+func LDHE() -> Void {
+    CPUStateInstance.registersState.h = CPUStateInstance.registersState.e;
+}
+
+// Put the value of H into H register 0x64
+func LDHH() -> Void {
+    CPUStateInstance.registersState.h = CPUStateInstance.registersState.h;
+}
+
+
+// Put the value of L into H register 0x65
+func LDHL() -> Void {
+    CPUStateInstance.registersState.h = CPUStateInstance.registersState.l;
+}
+
+// Put the value of A into H register 0x67
+func LDHA() -> Void {
+    CPUStateInstance.registersState.h = CPUStateInstance.registersState.a;
+}
+
+
+// Put the value of B into L register 0x68
+func LDLB() -> Void {
+    CPUStateInstance.registersState.l = CPUStateInstance.registersState.b;
+}
+
+// Put the value of C into L register 0x69
+func LDLC() -> Void {
+    CPUStateInstance.registersState.l = CPUStateInstance.registersState.c;
+}
+
+// Put the value of D into L register 0x6A
+func LDLD() -> Void {
+    CPUStateInstance.registersState.l = CPUStateInstance.registersState.d;
+}
+
+// Put the value of E into L register 0x6B
+func LDLE() -> Void {
+    CPUStateInstance.registersState.l = CPUStateInstance.registersState.e;
+}
+
+// Put the value of H into L register 0x6C
+func LDLH() -> Void {
+    CPUStateInstance.registersState.l = CPUStateInstance.registersState.h;
+}
+
+// Put the value of L into L register 0x6D
+func LDLL() -> Void {
+    CPUStateInstance.registersState.l = CPUStateInstance.registersState.l;
+}
+
+// Put the value of A into L register 0x6F
+func LDLA() -> Void {
+    CPUStateInstance.registersState.l = CPUStateInstance.registersState.a;
+}
+
+
 
 
 
@@ -1049,6 +1167,149 @@ func SUBA() -> Void {
     CPUStateInstance.registersState.f = 0b11000000;
     //SetFlagsRegister(z: 1 , n: 1, h: 0, c: 0)
 }
+
+
+// 0xA0 - 0xA5
+func ANDB() -> Void {
+    CPUStateInstance.registersState.a &= CPUStateInstance.registersState.b;
+    if CPUStateInstance.registersState.a == 0 {
+        CPUStateInstance.registersState.f = 0b10100000;
+    }
+    else {
+        CPUStateInstance.registersState.f = 0b00100000;
+    }
+}
+
+func ANDC() -> Void {
+    CPUStateInstance.registersState.a &= CPUStateInstance.registersState.c;
+    if CPUStateInstance.registersState.a == 0 {
+        CPUStateInstance.registersState.f = 0b10100000;
+    }
+    else {
+        CPUStateInstance.registersState.f = 0b00100000;
+    }
+}
+
+func ANDD() -> Void {
+    CPUStateInstance.registersState.a &= CPUStateInstance.registersState.d;
+    if CPUStateInstance.registersState.a == 0 {
+        CPUStateInstance.registersState.f = 0b10100000;
+    }
+    else {
+        CPUStateInstance.registersState.f = 0b00100000;
+    }
+}
+
+func ANDE() -> Void {
+    CPUStateInstance.registersState.a &= CPUStateInstance.registersState.e;
+    if CPUStateInstance.registersState.a == 0 {
+        CPUStateInstance.registersState.f = 0b10100000;
+    }
+    else {
+        CPUStateInstance.registersState.f = 0b00100000;
+    }
+}
+
+func ANDH() -> Void {
+    CPUStateInstance.registersState.a &= CPUStateInstance.registersState.h;
+    if CPUStateInstance.registersState.a == 0 {
+        CPUStateInstance.registersState.f = 0b10100000;
+    }
+    else {
+        CPUStateInstance.registersState.f = 0b00100000;
+    }
+}
+
+func ANDL() -> Void {
+    CPUStateInstance.registersState.a &= CPUStateInstance.registersState.l;
+    if CPUStateInstance.registersState.a == 0 {
+        CPUStateInstance.registersState.f = 0b10100000;
+    }
+    else {
+        CPUStateInstance.registersState.f = 0b00100000;
+    }
+}
+
+//0xA7
+func ANDA() -> Void {
+    if CPUStateInstance.registersState.a == 0 {
+        CPUStateInstance.registersState.f = 0b10100000;
+    }
+    else {
+        CPUStateInstance.registersState.f = 0b00100000;
+    }
+}
+
+
+// 0xB0 - 0xB5
+func ORB() -> Void {
+    CPUStateInstance.registersState.a |= CPUStateInstance.registersState.b;
+    if CPUStateInstance.registersState.a == 0 {
+        CPUStateInstance.registersState.f = 0b10000000;
+    }
+    else {
+        CPUStateInstance.registersState.f = 0b00000000;
+    }
+}
+
+func ORC() -> Void {
+    CPUStateInstance.registersState.a |= CPUStateInstance.registersState.c;
+    if CPUStateInstance.registersState.a == 0 {
+        CPUStateInstance.registersState.f = 0b10000000;
+    }
+    else {
+        CPUStateInstance.registersState.f = 0b00000000;
+    }
+}
+func ORD() -> Void {
+    CPUStateInstance.registersState.a |= CPUStateInstance.registersState.d;
+    if CPUStateInstance.registersState.a == 0 {
+        CPUStateInstance.registersState.f = 0b10000000;
+    }
+    else {
+        CPUStateInstance.registersState.f = 0b00000000;
+    }
+}
+
+func ORE() -> Void {
+    CPUStateInstance.registersState.a |= CPUStateInstance.registersState.e;
+    if CPUStateInstance.registersState.a == 0 {
+        CPUStateInstance.registersState.f = 0b10000000;
+    }
+    else {
+        CPUStateInstance.registersState.f = 0b00000000;
+    }
+}
+
+func ORH() -> Void {
+    CPUStateInstance.registersState.a |= CPUStateInstance.registersState.h;
+    if CPUStateInstance.registersState.a == 0 {
+        CPUStateInstance.registersState.f = 0b10000000;
+    }
+    else {
+        CPUStateInstance.registersState.f = 0b00000000;
+    }
+}
+
+func ORL() -> Void {
+    CPUStateInstance.registersState.a |= CPUStateInstance.registersState.l;
+    if CPUStateInstance.registersState.a == 0 {
+        CPUStateInstance.registersState.f = 0b10000000;
+    }
+    else {
+        CPUStateInstance.registersState.f = 0b00000000;
+    }
+}
+//0xB7
+func ORA() -> Void {
+    if CPUStateInstance.registersState.a == 0 {
+        CPUStateInstance.registersState.f = 0b10000000;
+    }
+    else {
+        CPUStateInstance.registersState.f = 0b00000000;
+    }
+}
+
 
 // 0xA8 - 0xAD
 func XORB() -> Void {
